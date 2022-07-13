839c386bc0455974e6cd555cebc353b0a356bb61
kgordeeva33 <101604852+kgordeeva33@users.noreply.github.com>
2022-07-13 17:38:22 -0400

    Add files via upload

---
+++ MyWidgetTest.spec.ts
@@ -0,0 +1,52 @@
+// Page Objet
+import { test, expect } from "@playwright/test";
+import { LoginPO } from "./pageobjects/LoginPO";
+import { MyUtilityPO } from "./pageobjects/MyAccount/MyUtilityPO";
+import { MyWidgetPO } from "./pageobjects/MyAccount/MyWidgetPO";
+
+// Page Data
+import { myWidgetPageData } from './pagedata/PageData'
+
+test.describe(`Verify Login Functionality`, () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto(`/`)
+        const Login = new LoginPO(page);
+
+        // Step 1: User login with valid credentials
+        await Login.enterLoginCredentialsAndClickOnLoginButton();
+
+        // Step 2: Verify home page URl
+        await expect(page.url()).toEqual("https://app.utilitygenius.com/")
+        await Login.verifyImageLogoIsDisplayed()
+
+    })
+    test(`Verify the My widget page functionality`, async ({ page }) => {
+
+        const MyUtility = new MyUtilityPO(page)
+        const MyWidget = new MyWidgetPO(page)
+
+        // Step 1: Select the My widget option of the Reports link and verify
+        await MyUtility.hoverToTheMyAccountLink()
+        await MyWidget.clickOnMyWidgetLink()
+
+        expect(page.url()).toEqual("https://app.utilitygenius.com/my-widgets")
+
+        // if the user has never agreed to the widget terms of agreement, agree
+        const agreeCheckbox = await page.locator('[type=checkbox]');
+
+        if(agreeCheckbox.isVisible()) {
+            agreeCheckbox.click();
+            const agreeButton = await page.locator('button:has-text("Agree")')
+            agreeButton.click()
+        }
+
+        // Step 2: Create new widgets
+        await MyWidget.clickOnNewWidetButton()
+        await MyWidget.createNewWidget(myWidgetPageData.widgetName, myWidgetPageData.widgetLocation)
+        await MyWidget.clickOnCloseButtonOfPopUp()
+        const widgetName = await MyWidget.getWidgetName()
+        expect(widgetName).toEqual(myWidgetPageData.widgetName)
+
+    })
+})
